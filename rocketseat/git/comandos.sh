7fb11d80c90d61debdbcfa8b8167120333dbbda5
Guilherme Maas <guilherme.maas@gmail.com>
2024-06-30 17:38:46 -0300

    Git Log

--- rocketseat/git/comandos.sh
+++ rocketseat/git/comandos.sh
@@ -6,6 +6,13 @@ git init --initial-branch main
 git status
 git add nome_arquivo
 git add .
+git commit -m "initial commit"
 
 #Remover do stage área/área de preparação
 git rm --cached fullcycle/docker/comandos.sh
+
+#Restaurar um arquivo
+git restore
+
+###Ver o log de commits e navegar entre eles para inspecionar o código
+git log
